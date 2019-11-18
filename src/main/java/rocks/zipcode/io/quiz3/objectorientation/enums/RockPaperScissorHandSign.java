14ffc4052b72fcd30c55df2d5a6e15f0b5191eb6
Jack Harris <jack@zipcoders-MacBook-Pro-49.local>
2019-11-18 17:16:39 -0500

    compiles again

--- src/main/java/rocks/zipcode/io/quiz3/objectorientation/enums/RockPaperScissorHandSign.java
+++ src/main/java/rocks/zipcode/io/quiz3/objectorientation/enums/RockPaperScissorHandSign.java
@@ -1,32 +1,16 @@
 package rocks.zipcode.io.quiz3.objectorientation.enums;
 
-import java.util.EnumMap;
-
 /**
  * @author leon on 09/12/2018.
  */
 public enum RockPaperScissorHandSign {
-    ROCK("ROCK"), PAPER("PAPER"), SCISSOR("SCISSOR");
+    ADD_ENUMERATIONS_HERE;
 
-    private String name;
-
-    RockPaperScissorHandSign (String name){
-        this.name = name;
-    }
-
-    public RockPaperScissorHandSign getWinner(Enum INPUT) {
-        if (this == ROCK && INPUT == SCISSOR){return ROCK;}
-        else if (this == SCISSOR && INPUT == PAPER){return SCISSOR;}
-        else if (this == PAPER && INPUT == ROCK){ return PAPER;}
+    public RockPaperScissorHandSign getWinner() {
         return null;
     }
 
-    public RockPaperScissorHandSign getLoser(Enum INPUT) {
-        if (this == ROCK && INPUT == SCISSOR){return SCISSOR;}
-        else if (this == SCISSOR && INPUT == PAPER){return PAPER;}
-        else if (this == PAPER && INPUT == ROCK){ return ROCK;}
+    public RockPaperScissorHandSign getLoser() {
         return null;
     }
-
-
 }
