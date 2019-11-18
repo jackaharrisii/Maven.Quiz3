998f9a2f25beffe60c195de3535f2ce25464cc95
Jack Harris <jack@zipcoders-MacBook-Pro-49.local>
2019-11-18 17:13:52 -0500

    update before leaving to teach class - we were never given an end time for this quiz

--- src/main/java/rocks/zipcode/io/quiz3/objectorientation/enums/RockPaperScissorHandSign.java
+++ src/main/java/rocks/zipcode/io/quiz3/objectorientation/enums/RockPaperScissorHandSign.java
@@ -1,16 +1,32 @@
 package rocks.zipcode.io.quiz3.objectorientation.enums;
 
+import java.util.EnumMap;
+
 /**
  * @author leon on 09/12/2018.
  */
 public enum RockPaperScissorHandSign {
-    ADD_ENUMERATIONS_HERE;
+    ROCK("ROCK"), PAPER("PAPER"), SCISSOR("SCISSOR");
 
-    public RockPaperScissorHandSign getWinner() {
+    private String name;
+
+    RockPaperScissorHandSign (String name){
+        this.name = name;
+    }
+
+    public RockPaperScissorHandSign getWinner(Enum INPUT) {
+        if (this == ROCK && INPUT == SCISSOR){return ROCK;}
+        else if (this == SCISSOR && INPUT == PAPER){return SCISSOR;}
+        else if (this == PAPER && INPUT == ROCK){ return PAPER;}
         return null;
     }
 
-    public RockPaperScissorHandSign getLoser() {
+    public RockPaperScissorHandSign getLoser(Enum INPUT) {
+        if (this == ROCK && INPUT == SCISSOR){return SCISSOR;}
+        else if (this == SCISSOR && INPUT == PAPER){return PAPER;}
+        else if (this == PAPER && INPUT == ROCK){ return ROCK;}
         return null;
     }
+
+
 }
