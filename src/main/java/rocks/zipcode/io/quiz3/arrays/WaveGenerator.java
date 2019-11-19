a360d54fcecb7760ca574125704005857b040af9
Jack Harris <jack@zipcoders-MacBook-Pro-49.local>
2019-11-18 20:09:42 -0500

    finished the waves, and rock/paper/scissors was stupid easy when I figured it out

--- src/main/java/rocks/zipcode/io/quiz3/arrays/WaveGenerator.java
+++ src/main/java/rocks/zipcode/io/quiz3/arrays/WaveGenerator.java
@@ -2,22 +2,35 @@ package rocks.zipcode.io.quiz3.arrays;
 
 import rocks.zipcode.io.quiz3.fundamentals.StringUtils;
 
+import java.util.ArrayList;
+
 /**
  * @author leon on 09/12/2018.
  */
 public class WaveGenerator {
+//    public static String[] wave(String str) {
+//        String[] waves = new String[str.length()];
+//        for (int i = 0; i < str.length(); i++) {
+//            waves[i] = str.toLowerCase();
+//            if (Character.isLetter(waves[i].charAt(i))) {
+//                waves[i] = StringUtils.capitalizeNthCharacter(waves[i], i);
+//            }
+//            if (i > countLetters(str)) {break;}
+//        }
+//        System.out.println(waves.toString());
+//        return waves;
+//    }
+
     public static String[] wave(String str) {
-        String[] waves = new String[str.length()];
-        for (int i = 0; i < str.length(); i++) {
-            waves[i] = str.toLowerCase();
-            if (Character.isLetter(waves[i].charAt(i))) {
-                waves[i] = StringUtils.capitalizeNthCharacter(waves[i], i);
+        ArrayList<String> waves = new ArrayList<>();
+        str = str.toLowerCase();
+        for (int i = 0; i < str.length(); i++){
+            if(Character.isLetter(str.charAt(i))){
+                waves.add(StringUtils.capitalizeNthCharacter(str,i));
             }
-            if (i > countLetters(str)) {break;}
         }
-        System.out.println(waves.toString());
-        return waves;
-    }
+    return waves.toArray(new String[waves.size()]);
+}
 
     public static Integer countLetters(String str) {
         Integer count = 0;
--- src/main/java/rocks/zipcode/io/quiz3/objectorientation/enums/RockPaperScissorHandSign.java
+++ src/main/java/rocks/zipcode/io/quiz3/objectorientation/enums/RockPaperScissorHandSign.java
@@ -4,13 +4,24 @@ package rocks.zipcode.io.quiz3.objectorientation.enums;
  * @author leon on 09/12/2018.
  */
 public enum RockPaperScissorHandSign {
-    ADD_ENUMERATIONS_HERE;
+    ROCK, PAPER, SCISSOR;
+
+    RockPaperScissorHandSign(){
+    }
 
     public RockPaperScissorHandSign getWinner() {
+        // if this == what they played, then the winning move is returned
+        if (this == ROCK){return PAPER;}
+        if (this == PAPER){return SCISSOR;}
+        if (this == SCISSOR){return ROCK;}
         return null;
     }
 
     public RockPaperScissorHandSign getLoser() {
+        // if this == what they played, then the losing move is returned
+        if (this == ROCK){return SCISSOR;}
+        if (this == PAPER){return ROCK;}
+        if (this == SCISSOR){return PAPER;}
         return null;
     }
 }
